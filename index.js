6344dad0c3e0c94b796cdc1655d6836aa7fa0573
C. DHANUSH KUMAR REDDY <156655062+Dhanushkumarreddy0204@users.noreply.github.com>
2025-07-12 13:19:29 +0530

    updating the test for the string

--- index.js
+++ index.js
@@ -17,3 +17,5 @@ arr.forEach(button => {
         }
         else if(e.target.innerHTML == 'DEL'){
             string = string.substring(0, string.length-1);
+            input.value = string;
+        }
